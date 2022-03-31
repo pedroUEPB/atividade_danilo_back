c1901c164d24867285b1d561409ab2c7d722264d
pedroandradee <pedro.h8237@gmail.com>
2022-03-31 14:06:31 -0300

    ajuste cors

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const port = process.env.PORT || 5000;
+const cors = require("cors")
 
+server.use(cors());
 server.use(router);
 
 server.listen(port, () => {
